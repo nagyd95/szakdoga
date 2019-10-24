5c0222249852d893b18746c701f2b97222ec3c75
Nagy Dávid <95david.nagy@gmail.com>
2019-10-24 19:43:18 +0200

    Login

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Bejelentkezés</title>
+  <link rel="stylesheet" type="text/css" href="css/login.css" />
+</head>
+<body>
+
+      
+      
+  
+  <div class="container">
+  <h1>PszeudoKód világa</h1>
+    <h2>Bejelentkezés</h2>
+
+    <form action="" method="post">
+      <div class="tbox">
+        <input type="text" name="" id="" placeholder="Felhasználónév">
+      </div>
+      <div class="tbox">
+        <input type="text" name="" id="" placeholder="Jelszó">
+      </div>
+
+      <input class="btn" type="submit" value="Bejelentkezés">
+      <input class="btn2" type="submit" value="Vissza">
+    <form>
+      <a class="b1" href="#">Elfelejtette jelszavát?</a>
+      <a href="signup.php" class="b2">Új felhasználó</a>
+  </div>
+</body>
+</html>
